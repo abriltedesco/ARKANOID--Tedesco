e66359b0f4020a6187082938191a0158a1b27bd1
Abril Tedesco <167026793+abriltedesco@users.noreply.github.com>
2026-03-20 00:45:37 -0300

    (primer intento de) creacion de menu
    me falta asociar el botón de jugar con el nodo de nivel 1 (por ahora no busqué como se hace) y obvio dejar la interfaz más linda ya que está bien basic por que estoy probando

---
+++ gds/menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_btn_j_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_btn_s_pressed() -> void:
+	get_tree().quit() #para salir
